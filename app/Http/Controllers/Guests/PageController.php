804564f4eedd65e5ef6de5837cda79159b2414bf
LeoneCarmelo <carmeloleone@outlook.it>
2023-05-25 15:29:50 +0200

    first commit

---
+++ app/Http/Controllers/Guests/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Guests;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    //
+    function index() {
+        return view('home');
+    }
+}
